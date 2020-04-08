22dfad5906198160d5835815892faaa8afafd013
Vladimir Nosov <vladimir@what.digital>
2020-04-08 13:16:49 +0700

    [master] added sound field in Card

--- apps/cards/graphql/types.py
+++ apps/cards/graphql/types.py
@@ -9,6 +9,10 @@ from apps.cards.models import LearningSession
 class CardType(DjangoObjectType):
     class Meta:
         model = Card
+        interfaces = (relay.Node,)
+
+    def resolve_sound(self: Card, info) -> str:
+        return info.context.build_absolute_uri(self.sound.url)
 
 
 class LearningSessionType(DjangoObjectType):
--- apps/cards/models/card.py
+++ apps/cards/models/card.py
@@ -1,8 +1,14 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
 class Card(models.Model):
     text = models.CharField(max_length=200)
+    sound = models.FileField(
+        validators=[
+            FileExtensionValidator(allowed_extensions=['mp3'])
+        ]
+    )
 
     class Meta:
         ordering = ('text',)
